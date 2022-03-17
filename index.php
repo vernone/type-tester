94e0bff0750cc112e60c62c7654bccb74fbbd555
Vernone <bouvier.simon61@gmail.com>
2022-03-17 13:14:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta property="og:url" content="" />
     <meta property="og:description" content="Type-tester est une site web permettant à chacun.e de mettre en scène et comparer différentes versions d'une typographie." />
   
-    <link rel="stylesheet" type="text/css" href="css/main.scss" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
     <link rel="stylesheet" type="text/css" href="css/button.css" />
     <link rel="stylesheet" type="text/css" href="css/normalize.css" />
 
@@ -27,13 +27,30 @@
     <div id="container">
     
       <div  id="container-loader"  class="container-vertical" >
+        <div id="wrapper_info" style="display:none">
+          <div id="wrapper_info_background"></div>
+          <div id="wrapper_info_text">
+            Type-tester est une plateforme web gratuite et open-source permettant à chacun.e de partager ses recherches de dessins typographiques.<br><br>
+
+            Reposant en partie sur la libraire opentype.js, ce site web permet d'afficher, de juxtaposer et superposer différents dessins typographiques à partir du chargement dans le site de vos fichiers .otf, .ttf, et .woff. Vous pouvez comparer le dessin via de multiples options (kerning, métrique, ligature, contour...) ainsi que la mise en forme dans une mise en page aux multiples échelles de textes.<br> <br>
+
+            Vous pouvez ensuite partager simplement ces états de recherches via un lien généré. Toutes personnes ayant reçu ce lien peut afficher les typographies précédemment chargées sur le site et les manipuler via la plateforme, tout cela en assurant une protection des fichiers du/de la dessinateurice.<br><br>
+
+            Les fichiers sont stockées anonymement durant une durée limitée sur le serveur afin de permettre la fonction de partage. Après cette durée, les fichiers sont supprimés et aucune trace ou copie n'est sauvegardée.<br>
+
+            D'autres fonctionnalités de manipulation, d'interaction, ainsi que des fonctions adjacentes à un projet d'élaboration d'un caractère viendront peu à peu.<br><br>
+
+            Design et développement&nbsp;: <a href="../">Simon Bouvier</a>.<br>
+            Typographie&nbsp;: Joanne par <a href="https://www.instagram.com/charlyderouault/">Charly Derouault</a>.<br>
+            Site rendu possible grâce à la bibliothèque <a href="https://opentype.js.org/">opentype.js</a>.
+          </div>
+        </div>  
         <div class="container-vertical">
           <div id="global-inputfile-wrapper">
             <div class="informations">
-                <span> TYPE TESTER </span>
+                <div id="button_info"> TYPE TESTER </div>
                 <span>Voir sur <a href="https://github.com/vernone/type-tester">Github</a></span>
             </div>
-
             <form enctype="multipart/form-data" action="upload.php" method="post">
               <div class="inputfile-wrapper">
                 <div class="inputfile-infos">
@@ -65,10 +82,11 @@
                   </div>
                 </div>
               </div>
-              <div class="addMore-share button_CB">
+              <div id="addMore-share">
                 <!-- <button class="add_more">Ajouter un autre fichier</button> -->
                 <input type="button" class="button" id="upload" value="Générez le lien à partager" />
-                <input type="text" id="link-tocopy" value="Hello world!">
+                  <input type="text" id="link-tocopy" value="Hello world!" style="display:none">
+                  <span id="link-tocopy-infos" style="display:none">Ce lien sera valable pendant 7 jours.</span>
               </div>
             </form>
           </div>
@@ -152,6 +170,12 @@
             <span>Mise en page</span>
         </span> -->
         <div id="mise-en-page" class="container-vertical">
+          <div class="inputfile-infos">
+            <input type="text" class="text-input" value="Grumpy wizards make toxic brew for the evil Queen and Jack. A quick movement of the enemy will jeopardize six gunboats. The job of waxing linoleum frequently peeves chintzy kids." id="textField-MEP" ></input>
+            <button class="button" id="textField-MPE-reset">Reset</button>
+
+
+          </div>
           <div class="mask">
               <div id="headlines02">
               </div>
@@ -161,7 +185,7 @@
                 </div>
               </div>
               
-              <button id="handle-btn" class="mask_handle " style="display:none;"><span class="icon-angle-left"></span>&nbsp;<span class="icon-angle-right"></span></button>
+              <button id="handle-btn" class="mask_handle" style="display:none;"><span class="icon-angle-left"></span>&nbsp;<span class="icon-angle-right"></span></button>
               </div>
             </div>
         </div>
@@ -169,9 +193,45 @@
 
     </div>
   </body>
-<script type="text/javascript" src="js/jquery-ui.js"></script>
 
-  <script>
+<script type="text/javascript" src="js/jquery-ui.js"></script>
+<script>
+    const textFieldMEP = document.querySelector('#textField-MEP');
+
+    $('body').on('click', '#textField-MPE-reset', function(e){
+        textFieldMEP.value ='Grumpy wizards make toxic brew for the evil Queen and Jack. A quick movement of the enemy will jeopardize six gunboats. The job of waxing linoleum frequently peeves chintzy kids.';
+        reloadAllMiseEnPage()
+    });
+
+    textFieldMEP.addEventListener('focus', (event) => {
+      event.target.style.width = '40%';
+      $("#textField-MPE-reset").hide()
+    });
+
+    textFieldMEP.addEventListener('blur', (event) => {
+      event.target.style.width = '';
+      $("#textField-MPE-reset").show()
+
+    });
+
+    $('#button_info').on('click', function(e){
+      toggleInfo();
+    });
+    $('#wrapper_info_background').click(function() {
+      toggleInfo();
+    });
+
+    function toggleInfo(){
+      var el = document.querySelectorAll('#wrapper_info');
+      var button = document.querySelector('#button_info');
+      if($(button).hasClass('show-item')){
+          $(button).toggleClass("show-item");
+          $(el).fadeOut()
+      }else{
+          $(button).toggleClass("show-item");
+          $(el).fadeIn()
+      } 
+    }
 
     Split(['#preview-container', '#mise-en-page'], {
       sizes: [35, 63],
@@ -216,8 +276,10 @@
             },
             success: function( data ) {
               // window.history.pushState("object or string", "Title", "index.php?id=" + data);
-              document.getElementById('link-tocopy').style.display = 'block';
-              document.getElementById('link-tocopy').value = "http://simon-bouvier.xyz/type-viewer/index.php?id=" + data;
+              document.getElementById("link-tocopy").style.display = 'block';
+              document.getElementById("link-tocopy-infos").style.display = 'block';
+
+              document.getElementById('link-tocopy').value = "http://simon-bouvier.xyz/type-tester/index.php?id=" + data;
             },
             error: function(xhr, status, error) {
               alert(xhr.responseText);
@@ -242,9 +304,21 @@
 <?php
   $out = array();
   $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
- 
+  $now = time();
+  $sevenDays = 60 * 60 * 24 * 7;
 
-  if ($url !== 'http://localhost/type-viewer/index.php' ){
+  if ( file_exists( 'uploads/' ) && is_dir( 'uploads/' ) ){
+    foreach (glob('uploads/' . '*') as $foldername) {
+      if (is_dir($foldername)){
+        if ($now - filemtime($foldername) >= $sevenDays) {
+          array_map('unlink', glob("$foldername/*.*"));
+          rmdir($foldername);
+        }
+      }
+    }
+  }
+
+  if ($url !== 'http://simon-bouvier.xyz/type-tester/' && $url !== 'http://simon-bouvier.xyz/type-tester/index.php'){
 
     $url_components = parse_url($url);
     parse_str($url_components['query'], $params);
@@ -272,7 +346,6 @@
 
 </script>
 
-<!-- <script src="bundle.js" charset="utf-8"></script> -->
 <script type="text/javascript" src="js/main.js"></script>
 <script type="text/javascript" src="js/scroll-horizontal.js"></script>
 
